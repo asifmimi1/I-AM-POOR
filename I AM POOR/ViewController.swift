667f427c8577677487d1e78912d1286652981b87
Asif MIMI <asifmimi@asifs-macbook.local>
2019-06-17 17:19:41 +0600

    Initial Commit

---
+++ I AM POOR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Asif MIMI on 17/6/19.
+//  Copyright © 2019 Asif MIMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
